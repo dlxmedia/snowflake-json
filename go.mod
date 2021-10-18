30af709b50e46731e964327fb13ed476e3e13ee3
Thomas Picquet <thomas@sctr.net>
2021-10-18 15:31:21 -0700

    Init

---
+++ go.mod
@@ -0,0 +1,7 @@
+module dlxmedia/snowflake
+
+go 1.16
+
+require (
+	github.com/bwmarrin/snowflake v0.3.0
+)
---
+++ snowflake-json.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+  "encoding/json"
+  "flag"
+	"fmt"
+	"github.com/bwmarrin/snowflake"
+)
+
+func main() {
+  snowflake.Epoch = 1545264000000
+
+  node, err := snowflake.NewNode(1)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  num := flag.Int("n", 5, "# of iterations")
+  flag.Parse()
+
+  n := *num
+
+  ids := make([]string, 0)
+	for i := 0; i < n; i++ {
+		ids = append(ids, node.Generate().String())
+	}
+
+  jsonResponse, err := json.Marshal(ids)
+	if err != nil {
+		panic(err)
+	}
+
+  fmt.Println(string(jsonResponse))
+}
